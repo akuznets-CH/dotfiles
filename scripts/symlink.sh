812aafad1a569a57a9cd815418bdc5eccee47465
Alex Kuznetsov <akuznetsov6@gatech.edu>
2025-09-01 19:47:45 -0400

    chore: add script to delete symlinks

--- scripts/symlink.sh
+++ scripts/symlink.sh
@@ -3,10 +3,19 @@
 # Get the directory of this script
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+echo "Creating symlinks from $DIR to $HOME..."
+
 # Create symlinks
+echo "Creating symlink for .vimrc..."
 ln -sfv "$DIR/.vimrc" "$HOME/.vimrc"
+
+echo "Creating symlink for .tmux.conf..."
 ln -sfv "$DIR/.tmux.conf" "$HOME/.tmux.conf"
+
+echo "Creating symlink for .config/nvim..."
 ln -sfnv "$DIR/.config/nvim" "$HOME/.config/nvim"
+
+echo "Creating symlink for .config/ghostty..."
 ln -sfnv "$DIR/.config/ghostty" "$HOME/.config/ghostty"
 
-echo "Done."
+echo "All symlinks created successfully."
