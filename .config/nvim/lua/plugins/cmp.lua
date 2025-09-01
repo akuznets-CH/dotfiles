9b03f1c4a0003f428710e3a69ba61af0dcb76929
Alex Kuznetsov <akuznetsov6@gatech.edu>
2025-09-01 15:23:33 -0400

    feat: switch to tab key for completions

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -27,7 +27,7 @@ return {
                 { name = "path" },
             }),
             mapping = {
-                ["<Tab>"] = cmp.mapping(function(fallback)
+                ["<C-n>"] = cmp.mapping(function(fallback)
                     if cmp.visible() then
                         cmp.select_next_item()
                     elseif luasnip.expand_or_jumpable() then
@@ -36,7 +36,7 @@ return {
                         fallback()
                     end
                 end, { "i", "s" }),
-                ["<S-Tab>"] = cmp.mapping(function(fallback)
+                ["<C-p>"] = cmp.mapping(function(fallback)
                     if cmp.visible() then
                         cmp.select_prev_item()
                     elseif luasnip.jumpable(-1) then
@@ -45,7 +45,7 @@ return {
                         fallback()
                     end
                 end, { "i", "s" }),
-                ["<CR>"] = cmp.mapping.confirm({ select = true }),
+                ["<Tab>"] = cmp.mapping.confirm({ select = true }),
                 ["<C-Space>"] = cmp.mapping.complete(),
             },
         })
