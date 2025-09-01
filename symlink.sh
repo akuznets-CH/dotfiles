d8ffb508e09de07b21f7a362757ea1fc1b686511
Alex Kuznetsov <akuznetsov6@gatech.edu>
2025-09-01 11:37:34 -0400

    fix: prevent duplicate symlinks

--- symlink.sh
+++ symlink.sh
@@ -6,7 +6,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 # Create symlinks
 ln -sfv "$DIR/.vimrc" "$HOME/.vimrc"
 ln -sfv "$DIR/.tmux.conf" "$HOME/.tmux.conf"
-ln -sfv "$DIR/.config/nvim" "$HOME/.config/nvim"
-ln -sfv "$DIR/.config/ghostty" "$HOME/.config/ghostty"
+ln -sfnv "$DIR/.config/nvim" "$HOME/.config/nvim"
+ln -sfnv "$DIR/.config/ghostty" "$HOME/.config/ghostty"
 
 echo "Done."
