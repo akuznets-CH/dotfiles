7429e1db53b0741c3f63472ad7b710ed276d8efd
Alex Kuznetsov <akuznetsov6@gatech.edu>
2025-09-01 11:22:45 -0400

    feat: symlink script

---
+++ symlink.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Get the directory of this script
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# Create symlinks
+ln -sfv "$DIR/.vimrc" "$HOME/.vimrc"
+ln -sfv "$DIR/.tmux.conf" "$HOME/.tmux.conf"
+ln -sfv "$DIR/.config/nvim" "$HOME/.config/nvim"
+
+echo "Done."
