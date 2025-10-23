7e77d700b3a03d4ece1231789ac47fa76b84f084
Alex Kuznetsov <alex.kuznetsov@charliehealth.com>
2025-10-23 13:56:28 -0400

    feat: kill tmux sessions from fzf

--- scripts/tmux-find-session.sh
+++ scripts/tmux-find-session.sh
@@ -4,7 +4,11 @@
 # If the user presses Esc, fzf exits with a non-zero status code,
 # and the script will exit thanks to `set -e`.
 set -e
-session=$(tmux list-sessions -F '#S' | fzf --reverse)
+session=$(tmux list-sessions -F '#S' | fzf \
+    --reverse \
+    --bind 'ctrl-k:execute(tmux kill-session -t {})' \
+    --bind 'ctrl-k:+reload(tmux list-sessions -F "#S")' \
+    --header 'Choose session (Ctrl-K to kill)')
 
 # If we're not already in a tmux session, attach to the selected session.
 # Otherwise, switch to it.
