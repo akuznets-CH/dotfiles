cbe1d31845ee6a045183eb71202c32c8ec7e2578
Alex Kuznetsov <alex.kuznetsov@charliehealth.com>
2025-10-23 14:00:50 -0400

    refactor: revert treesitter to load on open

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,7 +1,6 @@
 return {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
-    event = "VeryLazy",
     config = function()
         require("nvim-treesitter.configs").setup({
             ensure_installed = {
