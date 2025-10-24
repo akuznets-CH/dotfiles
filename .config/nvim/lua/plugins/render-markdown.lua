e5649bfba3595e08d76b2571f4d5938d941b19cb
Alex Kuznetsov <alex.kuznetsov@charliehealth.com>
2025-10-24 09:48:49 -0400

    feat: remove render-markdown

--- .config/nvim/lua/plugins/render-markdown.lua
+++
@@ -1,9 +0,0 @@
-
-return {
-    "MeanderingProgrammer/render-markdown.nvim",
-    event = "VeryLazy",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    config = function()
-        require("render-markdown").setup({})
-    end,
-}
