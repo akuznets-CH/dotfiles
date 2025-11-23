6729c69ebbe31b395b0d0d302771f1fa8d955e54
Alex Kuznetsov <akuznets96@gmail.com>
2025-11-23 03:02:30 -0500

    feat: use official copilot plugin

--- src/nvim/nvim/lua/plugins/copilot.lua
+++ src/nvim/nvim/lua/plugins/copilot.lua
@@ -1,21 +1,3 @@
 return {
-    {
-        "zbirenbaum/copilot.lua",
-        cmd = "Copilot",
-        event = "VeryLazy",
-        config = function()
-            require("copilot").setup({
-                suggestions = { enabled = false },
-                panel = { enabled = false },
-            })
-        end,
-    },
-    {
-        "zbirenbaum/copilot-cmp",
-        dependencies = { "zbirenbaum/copilot.lua" },
-        event = "VeryLazy",
-        config = function()
-            require("copilot_cmp").setup()
-        end,
-    },
+    "github/copilot.vim"
 }
